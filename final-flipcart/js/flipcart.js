551a36afd615763130fc81829cdfeb117bc9b9bf
ayushi995 <82277175+ayushi995@users.noreply.github.com>
2023-01-05 12:28:26 +0530

    final-flipkart

---
+++ final-flipcart/js/flipcart.js
@@ -0,0 +1,344 @@
+var arr = [
+	{
+		id:1,
+		categories:"brockrade suit",
+		color:"red",
+		net_Price:1400,
+		actual_price:550,
+		discount:65,
+		rating:3,
+		image:"https://rukminim1.flixcart.com/image/325/390/k5o7r0w0/fabric/q/z/h/ej4002-1006-banarasi-green-ethnicjunction-original-imafzavtrrggbcsh.jpeg?q=50",
+	},
+	{
+		id:2,
+		categories:"brockrade suit",
+		color:"pink",
+		net_Price:1200,
+		actual_price:700,
+		discount:40,
+		rating:4,
+		image:"https://rukminim1.flixcart.com/image/325/390/kd7pevk0/fabric/0/c/9/yes-unstitched-bb-10007-d-being-banarasi-original-imafu5wn9cpvupke.jpeg?q=50",
+	},
+	{
+		id:3,
+		categories:"brockrade suit",
+		color:"blue",
+		net_Price:1500,
+		actual_price:900,
+		discount:45,
+		rating:4,
+		image:"https://rukminim1.flixcart.com/image/325/390/kd7pevk0/fabric/a/c/k/yes-unstitched-bb-10009-b-being-banarasi-original-imafu5wmrbq98vpd.jpeg?q=50",
+	},
+	{
+		
+		id:4,
+		categories:"brockrade suit",
+		color:"blue",
+		net_Price:1350,
+		actual_price:680,
+		discount:55,
+		rating:3,
+		image:"https://rukminim1.flixcart.com/image/325/390/kd7pevk0/fabric/m/t/q/yes-unstitched-bb-10009-a-being-banarasi-original-imafu5wmxxezmsef.jpeg?q=50",
+	},
+	{
+		id:5,
+		categories:"brockrade suit",
+		color:"orange",
+		net_Price:1350,
+		actual_price:680,
+		discount:55,
+		rating:3,
+		image:"https://rukminim1.flixcart.com/image/325/390/kd7pevk0/fabric/4/g/f/yes-unstitched-bb-10007-a-being-banarasi-original-imafu5wngfg6zxzh.jpeg?q=50",
+	},
+	{
+		id:6,
+		categories:"brockrade suit",
+		color:"pink",
+		net_Price:1000,
+		actual_price:780,
+		discount:35,
+		rating:2,
+		image:"https://rukminim1.flixcart.com/image/325/390/k5o7r0w0/fabric/g/2/v/ej4002-1010-banarasi-pink-ethnicjunction-original-imafzavtgd4fgtce.jpeg?q=50",
+	},
+	{
+		id:7,
+		categories:"brockrade suit",
+		color:"orange",
+		net_Price:1200,
+		actual_price:500,
+		discount:48,
+		rating:2,
+		image:"https://rukminim1.flixcart.com/image/325/390/kd7pevk0/fabric/s/i/0/yes-unstitched-bb-10007-e-being-banarasi-original-imafu5wndajn9gkq.jpeg?q=50",
+	},
+	{
+		id:8,
+		categories:"plazo with kurta",
+		color:"black",
+		net_Price:900,
+		actual_price:450,
+		discount:50,
+		rating:3,
+		image:"https://rukminim1.flixcart.com/image/325/390/kflftzk0/ethnic-set/x/c/j/m-ikrt0180-idalia-original-imafwfyzababdygh.jpeg?q=50",
+	},
+	{
+		id:9,
+		categories:"plazo with kurta",
+		color:"black",
+		net_Price:700,
+		actual_price:550,
+		discount:30,
+		rating:2,
+		image:"https://rukminim1.flixcart.com/image/325/390/kfmv9u80/ethnic-set/c/x/r/38-nmk1064-nehamta-original-imafwfgcvhbyz6mp.jpeg?q=50",
+	},
+	{
+		id:9,
+		categories:"plazo with kurta",
+		color:"red",
+		net_Price:1000,
+		actual_price:500,
+		discount:50,
+		rating:4,
+		image:"https://rukminim1.flixcart.com/image/325/390/kfmv9u80/ethnic-set/h/f/a/l-nmk1117-nehamta-original-imafwfgcnazq4rbk.jpeg?q=50",
+	},
+	{
+		id:10,
+		categories:"plazo with kurta",
+		color:"black",
+		net_Price:600,
+		actual_price:500,
+		discount:15,
+		rating:1,
+		image:"https://rukminim1.flixcart.com/image/325/390/kfu0h3k0/ethnic-set/h/r/y/xl-green-chokdi-navya-print-original-imafw7n5athgme9e.jpeg?q=50",
+	},
+	{
+		id:11,
+		categories:"plazo with kurta",
+		color:"black",
+		net_Price:900,
+		actual_price:450,
+		discount:50,
+		rating:3,
+		image:"https://rukminim1.flixcart.com/image/325/390/kfu0h3k0/ethnic-set/h/r/y/xl-green-chokdi-navya-print-original-imafw7n5athgme9e.jpeg?q=50",
+	},
+	{
+		id:12,
+		categories:"plazo with kurta",
+		color:"blue",
+		net_Price:770,
+		actual_price:470,
+		discount:40,
+		rating:2,
+		image:"https://rukminim1.flixcart.com/image/325/390/kflftzk0/ethnic-set/5/3/x/s-254k244-29tk107-saara-original-imafwfyzwkp8ajh9.jpeg?q=50",
+	},
+	{
+		id:13,
+		categories:"plazo with kurta",
+		color:"white",
+		net_Price:900,
+		actual_price:500,
+		discount:50,
+		rating:3,
+		image:"https://rukminim1.flixcart.com/image/325/390/kfmv9u80/ethnic-set/f/d/f/l-white-kp-l-fashioncutz-original-imafwfg7nkkkzzsn.jpeg?q=50",
+	},
+	{
+		id:14,
+		categories:"plazo with kurta",
+		color:"blue",
+		net_Price:1120,
+		actual_price:600,
+		discount:50,
+		rating:4,
+		image:"https://rukminim1.flixcart.com/image/325/390/kfmv9u80/ethnic-set/9/h/b/l-sk33-trilokesh-original-imafwfg7qtrfg5gt.jpeg?q=50",
+	},
+	{
+		id:15,
+		categories:"plazo with kurta",
+		color:"yellow",
+		net_Price:850,
+		actual_price:400,
+		discount:45,
+		rating:2,
+		image:"https://rukminim1.flixcart.com/image/325/390/kfmv9u80/ethnic-set/h/b/x/xxl-yellowdupattaset-jagdamba-fashion-original-imafwfgbfzybcbfd.jpeg?q=50",
+	},
+	{
+		id:16,
+		categories:"tops",
+		color:"black",
+		net_Price:700,
+		actual_price:500,
+		discount:30,
+		rating:4,
+		image:"https://rukminim1.flixcart.com/image/325/390/kgwld3k0/top/j/f/p/s-bhavya-179-absorbing-original-imafxfjv4hqdh35k.jpeg?q=50",
+	},
+	{
+		id:17,
+		categories:"tops",
+		color:"black",
+		net_Price:580,
+		actual_price:450,
+		discount:70,
+		rating:2,
+		image:"https://rukminim1.flixcart.com/image/325/390/kigbjbk0-0/top/e/h/q/l-bw-2001-tani-black-badger-winsome-original-imafy8wp9ht5zhmv.jpeg?q=50",
+	},
+	{
+		id:18,
+		categories:"tops",
+		color:"white",
+		net_Price:600,
+		actual_price:450,
+		discount:78,
+		rating:3,
+		image:"https://rukminim1.flixcart.com/image/325/390/kflftzk0/top/h/h/e/m-dn-kt79-duenite-original-imafwyuywhtfr6jz.jpeg?q=50",
+	},
+	{
+		id:19,
+		categories:"tops",
+		color:"black",
+		net_Price:1000,
+		actual_price:750,
+		discount:30,
+		rating:4,
+		image:"https://rukminim1.flixcart.com/image/325/390/kflftzk0/top/h/x/z/s-2447-buynewtrend-original-imafwyzkdjtzhucg.jpeg?q=50",
+	},
+	{
+		id:20,
+		categories:"tops",
+		color:"yellow",
+		net_Price:600,
+		actual_price:450,
+		discount:60,
+		rating:2,
+		image:"https://rukminim1.flixcart.com/image/325/390/kflftzk0/top/j/6/z/s-rr-tp193-blezza-original-imafwyzhs3ekwqgf.jpeg?q=50",
+	},
+	{
+		id:21,
+		categories:"tops",
+		color:"yellow",
+		net_Price:580,
+		actual_price:400,
+		discount:67,
+		rating:2,
+		image:"https://rukminim1.flixcart.com/image/325/390/kflftzk0/top/j/y/2/s-aw-4019md-aelomart-original-imafwysfrghgszfm.jpeg?q=50",
+	},	
+	{
+		id:22,
+		categories:"tops",
+		color:"brinjal",
+		net_Price:700,
+		actual_price:530,
+		discount:59,
+		rating:3,
+		image:"https://rukminim1.flixcart.com/image/325/390/kflftzk0/top/z/g/z/m-dn-kt69-duenite-original-imafwyzjrprffy2p.jpeg?q=50",
+	},	
+	{
+		id:23,
+		categories:"tops",
+		color:"blue",
+		net_Price:800,
+		actual_price:400,
+		discount:50,
+		rating:3,
+		image:"https://rukminim1.flixcart.com/image/325/390/kflftzk0/top/4/e/9/xl-3011-the-dry-state-original-imafwyzyzq4cevrr.jpeg?q=50",
+	},	
+	{
+		id:24,
+		categories:"tops",
+		color:"pink",
+		net_Price:700,
+		actual_price:500,
+		discount:40,
+		rating:4,
+		image:"https://rukminim1.flixcart.com/image/325/390/kflftzk0/top/y/q/3/xl-aw-4019dm-aelomart-original-imafwyzgskfcpxwf.jpeg?q=50",
+	},	
+	{
+		id:25,
+		categories:"tops",
+		color:"black",
+		net_Price:900,
+		actual_price:700,
+		discount:30,
+		rating:4,
+		image:"https://rukminim1.flixcart.com/image/325/390/kflftzk0/top/d/6/b/m-mcpf13tp01-15-62-miss-chase-original-imafwyv44rpnfg3a.jpeg?q=50",
+	},	
+	{
+		id:26,
+		categories:"tops",
+		color:"black",
+		net_Price:750,
+		actual_price:550,
+		discount:40,
+		rating:4,
+		image:"https://rukminim1.flixcart.com/image/325/390/kigbjbk0-0/top/e/h/q/l-bw-2001-tani-black-badger-winsome-original-imafy8wp9ht5zhmv.jpeg?q=50",
+	},	
+	{
+		id:27,
+		categories:"tops",
+		color:"yellow",
+		net_Price:600,
+		actual_price:500,
+		discount:10,
+		rating:2,
+		image:"https://rukminim1.flixcart.com/image/325/390/kflftzk0/top/r/9/m/xxl-dn-kt68-duenite-original-imafwyzjpqmna4yf.jpeg?q=50",
+	},		
+];
+
+
+function showlist(arr){
+	
+	document.getElementById("lists").innerHTML = "";
+	
+	var map = arr.map((item, index) => {
+		var boxEl = document.createElement("div");
+		boxEl.setAttribute("class", "box");
+		
+		var imageEl = document.createElement("div");
+		imageEl.setAttribute("class", "image");
+
+		var img = document.createElement("img");
+		img.setAttribute("src", item.image);
+		imageEl.appendChild(img);
+		boxEl.appendChild(imageEl);
+		
+		var contentEl = document.createElement("div");
+		contentEl.setAttribute("class", "content");
+		var category = document.createElement("h1")
+		category.innerHTML = item.categories;
+		contentEl.appendChild(category);
+		
+		var color = document.createElement("h2")
+		color.innerHTML = item.color;
+		contentEl.appendChild(color);
+		
+		var fixPrice = document.createElement("span");
+		fixPrice.setAttribute("class", "one");
+		fixPrice.innerHTML = "Rs" + item.actual_price;
+		contentEl.appendChild(fixPrice);
+		
+		var netPrice = document.createElement("span");
+		netPrice.setAttribute("class", "two");
+		netPrice.innerHTML = item.net_Price + "Rs";
+		contentEl.appendChild(netPrice);
+		
+		
+		
+		var disPrice = document.createElement("h3");
+		disPrice.innerHTML = item.discount + "% off";
+		contentEl.appendChild(disPrice);
+
+		boxEl.appendChild(contentEl);
+		
+		document.getElementById("lists").appendChild(boxEl);
+	});
+}
+
+showlist(arr);
+
+
+function search(searchkey){
+	var filterarr = arr.filter((item, index) => item.categories.match(searchkey) || item.color.match(searchkey));
+	showlist(filterarr);
+}
+
+function seachclick(){
+	document.getElementById("search").value = "";
+}
